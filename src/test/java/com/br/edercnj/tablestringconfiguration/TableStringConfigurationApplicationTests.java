5cb5285b7c9cbfd743447bb6fb22352be1315ff7
Eder Celeste Nunes Júnior <edercnj@gmail.com>
2021-08-31 09:58:42 -0300

    "crate project"

---
+++ src/test/java/com/br/edercnj/tablestringconfiguration/TableStringConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.edercnj.tablestringconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableStringConfigurationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
